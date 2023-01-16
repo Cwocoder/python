d216277c4922a6e246aee24b61e87c1d0e570cad
Robert C <103590074+Cwocoder@users.noreply.github.com>
2023-01-16 14:25:10 +0800

    网课练习

---
+++ 求1~100所有偶数的和.py
@@ -0,0 +1,8 @@
+# 2、求1~100所有偶数的和
+i=0
+sum=0
+while i<100:
+    i += 1
+    if i%2==0:
+        sum+=i
+print(sum)
---
+++ 求1~100的和.py
@@ -0,0 +1,7 @@
+# 1、求1~100的和
+i=0
+sum=0
+while i<100:
+    i += 1
+    sum+=i
+print(sum)
